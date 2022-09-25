ef1b910629da09ff026d81bcb3dea4a29ba2b701
abhi4u80 <abhijisha2009@gmail.com>
2022-09-25 14:09:34 +0530

    End ofsection 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public Guid RegionID { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        // navigation properties
+
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
